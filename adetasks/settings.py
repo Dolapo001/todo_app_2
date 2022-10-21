abef1eddee2a1ae454024454b56edc6bec723954
Dolapo24 <adedolapovictoria927@gmail.com>
2022-10-21 17:42:32 +0100

    deploying now

--- adetasks/settings.py
+++ adetasks/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ["adetasks.herokuapp.com", "localhost", "127.0.0.1"]
 
 # Application definition
@@ -124,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = \
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
