5a259bd9e61192ed4b858000d898b28c14491dbe
Dolapo24 <adedolapovictoria927@gmail.com>
2022-10-21 17:13:55 +0100

    MAIN

--- adetasks/asgi.py
+++ adetasks/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoProject7 project.
+ASGI config for adetasks project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject7.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adetasks.settings')
 
 application = get_asgi_application()
--- adetasks/wsgi.py
+++ adetasks/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoProject7 project.
+WSGI config for adetasks project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject7.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adetasks.settings')
 
 application = get_wsgi_application()
